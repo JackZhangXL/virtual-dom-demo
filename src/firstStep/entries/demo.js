fa51348d29a9e17c47cd68c66445964fd34d7d9e
zhangxinlin <zhangxinlin@meituan.com>
2017-08-02 19:22:23 +0800

    update

--- src/firstStep/entries/demo.js
+++ src/firstStep/entries/demo.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Element from '../lib/element';
+import Diff from '../lib/diff';
+import Patch from '../lib/patch';
 
 import './demo.pcss';
 
 export default class Demo extends Component {
-
     render() {
         return (
             <div className="wrap">
-                <h3>Real DOM</h3>
                 <div id="realDom">
+                    <h3>Real DOM</h3>
                     <ul id="list1">
                         <li className="item">Item 1</li>
                         <li className="item">Item 2</li>
                         <li className="item">Item 3</li>
                     </ul>
                 </div>
-                <h3>Virtual DOM</h3>
                 <div id="virtualDom" />
             </div>
         );
@@ -30,14 +30,33 @@ render(
 );
 
 const renderVirtualDom = () => {
-    const ul = Element('ul', {id: 'list2'}, [
-        Element('li', {class: 'item'}, ['Item 1']),
-        Element('li', {class: 'item'}, ['Item 2']),
-        Element('li', {class: 'item'}, ['Item 3']),
+    const tree = Element('div', {'id': 'container'}, [
+        Element('h3', ['Virtual DOM']),
+        Element('p', ['before update']),
+        Element('ul', [
+            Element('li', { class: 'item' }, ['Item 1']),
+            Element('li', { class: 'item' }, ['Item 2']),
+            Element('li', { class: 'item' }, ['Item 3']),
+        ]),
     ]);
 
-    const ulRoot = ul.render();
-    document.getElementById('virtualDom').appendChild(ulRoot)
+    const root = tree.render();
+    document.getElementById('virtualDom').appendChild(root);
+
+    const newTree = Element('div', {'id': 'container'}, [
+        Element('h3', {style: 'color: red'}, ['Virtual DOM']),
+        Element('p', {style: 'color: red'}, ['after update']),
+        Element('ul', [
+            Element('li', { class: 'item red' }, ['Update Item 1']),
+            Element('li', { class: 'item red' }, ['Update Item 2']),
+            Element('li', { class: 'item red' }, ['Update Item 3']),
+        ]),
+    ]);
+
+    setTimeout(() => {
+        const patches = Diff(tree, newTree);
+        Patch(root, patches);
+    }, 2000);
 };
 
 renderVirtualDom();
