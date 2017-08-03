eb79c600044558c4b22a3ce9b325c7540af74496
zhangxinlin <zhangxinlin@meituan.com>
2017-08-03 13:57:14 +0800

    update

--- src/firstStep/lib/diff.js
+++ src/firstStep/lib/diff.js
@@ -1,12 +1,51 @@
-import _ from './util';
-import patch from './patch';
 import listDiff from 'list-diff2';
+import patch from './patch';
 
-function diff(oldTree, newTree) {
-    const index = 0;
-    const patches = {};
-    walk(oldTree, newTree, patches, index);
-    return patches;
+function diffChildren(oldChildren, newChildren, patches, index, currentPatch) {
+    var diffs = listDiff(oldChildren, newChildren, 'key')
+    newChildren = diffs.children
+
+    if (diffs.moves.length) {
+        const reorderPatch = { type: patch.REORDER, moves: diffs.moves };
+        currentPatch.push(reorderPatch);
+    }
+
+    let leftNode = null;
+    let currentNodeIndex = index;
+    oldChildren.forEach((child, i) => {
+        currentNodeIndex = (leftNode && leftNode.count)
+            ? currentNodeIndex + leftNode.count + 1
+            : currentNodeIndex + 1;
+        walk(child, newChildren[i], patches, currentNodeIndex);
+        leftNode = child;
+    });
+}
+
+function diffProps(oldNode, newNode) {
+    const oldProps = oldNode.props;
+    const newProps = newNode.props;
+
+    let key;
+    const propsPatches = {};
+    let isSame = true;
+
+    // find out different props
+    for (key in oldProps) {         // eslint-disable-line
+        if (newProps[key] !== oldProps[key]) {
+            isSame = false;
+            propsPatches[key] = newProps[key];
+        }
+    }
+
+    // find out new props
+    for (key in newProps) {         // eslint-disable-line
+        if (!oldProps.hasOwnProperty(key)) {
+            isSame = false;
+            propsPatches[key] = newProps[key];
+        }
+    }
+
+    return isSame ? null : propsPatches;
 }
 
 function walk(oldNode, newNode, patches, index) {
@@ -14,100 +53,47 @@ function walk(oldNode, newNode, patches, index) {
         return;
     }
 
-    let currentPatch = [];
+    const currentPatch = [];
 
     // Node is removed.
     if (newNode === null) {
         // Real DOM node will be removed when perform reordering, so has no needs to do anythings in here
-        // TextNode content replacing
     } else if (typeof oldNode === 'string' && typeof newNode === 'string') {
+        // TextNode content replacing
         if (newNode !== oldNode) {
-            currentPatch.push({ type: patch.TEXT, content: newNode })
+            currentPatch.push({ type: patch.TEXT, content: newNode });
         }
-        // Nodes are the same, diff old node's props and children
     } else if (oldNode.tagName === newNode.tagName && oldNode.key === newNode.key) {
+        // Nodes are the same, diff old node's props and children
         // Diff props
-        var propsPatches = diffProps(oldNode, newNode)
+        const propsPatches = diffProps(oldNode, newNode);
         if (propsPatches) {
-        currentPatch.push({ type: patch.PROPS, props: propsPatches })
+            console.log('propsPatches', propsPatches);
+            currentPatch.push({ type: patch.PROPS, props: propsPatches });
         }
         // Diff children. If the node has a `ignore` property, do not diff children
-        if (!isIgnoreChildren(newNode)) {
-        diffChildren(
-        oldNode.children,
-        newNode.children,
-        index,
-        patches,
-        currentPatch
-        )
-        }
-        // Nodes are not the same, replace the old node with new node
+        // if (!isIgnoreChildren(newNode)) {
+        //     diffChildren(oldNode.children, newNode.children, patches, index, currentPatch);
+        // }
+        diffChildren(oldNode.children, newNode.children, patches, index, currentPatch);
     } else {
-        currentPatch.push({ type: patch.REPLACE, node: newNode })
+        // Nodes are not the same, replace the old node with new node
+        currentPatch.push({ type: patch.REPLACE, node: newNode });
     }
 
     if (currentPatch.length) {
-        patches[index] = currentPatch
+        patches[index] = currentPatch;
     }
 }
 
-function diffChildren (oldChildren, newChildren, index, patches, currentPatch) {
-  var diffs = listDiff(oldChildren, newChildren, 'key')
-  newChildren = diffs.children
-
-  if (diffs.moves.length) {
-    var reorderPatch = { type: patch.REORDER, moves: diffs.moves }
-    currentPatch.push(reorderPatch)
-  }
-
-  var leftNode = null
-  var currentNodeIndex = index
-    oldChildren.forEach((child, i) => {
-    var newChild = newChildren[i]
-    currentNodeIndex = (leftNode && leftNode.count)
-      ? currentNodeIndex + leftNode.count + 1
-      : currentNodeIndex + 1
-    walk(child, newChild, patches, currentNodeIndex)
-    leftNode = child
-  });
+function diff(oldTree, newTree) {
+    const index = 0;
+    const patches = {};
+    walk(oldTree, newTree, patches, index);
+    return patches;
 }
-
-function diffProps (oldNode, newNode) {
-  var count = 0
-  var oldProps = oldNode.props
-  var newProps = newNode.props
-
-  var key, value
-  var propsPatches = {}
-
-  // Find out different properties
-  for (key in oldProps) {
-    value = oldProps[key]
-    if (newProps[key] !== value) {
-      count++
-      propsPatches[key] = newProps[key]
-    }
-  }
-
-  // Find out new property
-  for (key in newProps) {
-    value = newProps[key]
-    if (!oldProps.hasOwnProperty(key)) {
-      count++
-      propsPatches[key] = newProps[key]
-    }
-  }
-
-  // If properties all are identical
-  if (count === 0) {
-    return null
-  }
-
-  return propsPatches
-}
-
-function isIgnoreChildren (node) {
-  return (node.props && node.props.hasOwnProperty('ignore'))
-}
+// function isIgnoreChildren (node) {
+//     return (node.props && node.props.hasOwnProperty('ignore'));
+// }
 
 export default diff;
